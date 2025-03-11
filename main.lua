c11ab05b21f43d4acdbec5524e8abb4fc5a1cf7d
lpnh <paniguel.lpnh@gmail.com>
2025-03-11 10:11:22 -0300

    refactor: avoid code repetition

--- main.lua
+++ main.lua
@@ -30,7 +30,7 @@ local fmt_opts = function(opt)
 	end
 	return ""
 end
-local get_custom_opts = ya.sync(function(self)
+local get_user_opts = ya.sync(function(self)
 	local opts = self.custom_opts or {}
 
 	return {
@@ -51,7 +51,6 @@ local function eza_preview(prev_type, opts)
 		[[echo -e "\x1b[38;2;148;130;158m────────────────────────────────────────────────────────────────────────────────\x1b[m";]]
 	local bar_n =
 		[[echo -e "\n\x1b[38;2;148;130;158m────────────────────────────────────────────────────────────────────────────────\x1b[m";]]
-
 	local dir_or_file_name = {
 		default = [[echo -ne "Dir: \x1b[1m\x1b[38m{}\x1b[m";]],
 		meta = [[test -d {1} && echo -ne "Dir: \x1b[1m\x1b[38m{1}\x1b[m"]]
@@ -70,34 +69,72 @@ local function eza_preview(prev_type, opts)
 		bar_n,
 		"eza",
 		extra_flags[prev_type],
-		"--color=always --group-directories-first --icons {" .. "1" .. "};",
+		"--color=always --group-directories-first --icons {1};",
 		bar,
 	}, " ")
 end
 
+-- common `fzf` base cmd
+local function build_from_fzf_base(search_cmd, preview_cmd, preview_window, prompt, user_opts, specific_options)
+	local base_tbl = {
+		"fzf",
+		"--ansi",
+		"--no-multi",
+		"--reverse",
+		"--preview-label='content'",
+		string.format("--bind='start:reload:%s'", search_cmd),
+		string.format("--bind='change:reload:sleep 0.1; %s || true'", search_cmd),
+		string.format("--bind='ctrl-r:clear-query+reload:%s || true'", search_cmd),
+		string.format("--prompt='%s'", prompt),
+		string.format("--preview='%s'", preview_cmd),
+		string.format("--preview-window='%s'", preview_window),
+		"--bind='ctrl-]:change-preview-window(80%|66%)'",
+		"--bind='ctrl-\\:change-preview-window(right|up)'",
+		string.format(
+			"--bind='alt-c:change-preview-label(content)+change-preview-window(~3)+change-preview:%s'",
+			preview_cmd
+		),
+		string.format(
+			"--bind='alt-m:change-preview-label(metadata)+change-preview-window(~5)+change-preview(%s)'",
+			eza_preview("meta", user_opts)
+		),
+	}
+
+	for _, option in ipairs(specific_options) do
+		table.insert(base_tbl, option)
+	end
+
+	-- add `fzf` custom options from user setup
+	table.insert(base_tbl, user_opts.fzf)
+
+	return base_tbl
+end
+
 -- fzf with `rg` or `rga` search
-local function build_search_by_content(search_type, opts)
+local function build_search_by_content(search_type, user_opts)
 	local sh = get_sh_helper()
 	local cmd_tbl = {
 		rg = {
-			grep = "rg --color=always --line-number --smart-case" .. opts.rg,
+			grep = "rg --color=always --line-number --smart-case" .. user_opts.rg .. " {q}",
 			-- https://github.com/junegunn/fzf/blob/aefb9a5bc41f92227e4bffa050caca0270b450ba/man/man1/fzf.1#L932-L943
-			prev = "bat --color=always " .. opts.bat .. " --highlight-line={2} {1}",
+			prev = "bat --color=always " .. user_opts.bat .. " --highlight-line={2} {1}",
 			prev_window = "~3,+{2}+3/2,up,66%",
-			prompt = "--prompt='rg> '",
+			prompt = "rg> ",
+			specific_options = { "--disabled", "--bind='ctrl-o:execute:$EDITOR {1} +{2}'", "--delimiter=:", "--nth=3.." },
 			fzf_match = function(cmd_grep)
 				-- fzf match option <ctrl-s>
 				local bind_fzf_match_tmpl = "--bind='ctrl-s:transform:%s "
-					.. [[echo "rebind(change)+change-prompt(rg> )+disable-search+clear-query+reload(%s {q} || true)" %s ]]
+					.. [[echo "rebind(change)+change-prompt(rg> )+disable-search+clear-query+reload:%s || true" %s ]]
 					.. [[echo "unbind(change)+change-prompt(fzf> )+enable-search+clear-query"']]
 				return string.format(bind_fzf_match_tmpl, sh.rg_prompt.cond, cmd_grep, sh.rg_prompt.op)
 			end,
 		},
 		rga = {
-			grep = "rga --color=always --files-with-matches --smart-case" .. opts.rga,
-			prev = "rga --context 5 --no-messages --pretty " .. opts.rga_preview .. " {q} {}",
+			grep = "rga --color=always --files-with-matches --smart-case" .. user_opts.rga .. " {q}",
+			prev = "rga --context 5 --no-messages --pretty " .. user_opts.rga_preview .. " {q} {}",
 			prev_window = "up,66%",
-			prompt = "--prompt='rga> '",
+			prompt = "rga> ",
+			specific_options = { "--disabled", "--bind='ctrl-o:execute:$EDITOR {}'" },
 		},
 	}
 
@@ -107,51 +144,26 @@ local function build_search_by_content(search_type, opts)
 		return nil
 	end
 
-	local fzf_tbl = {
-		"fzf",
-		"--ansi",
-		"--no-multi",
-		"--reverse",
-		"--delimiter=:",
-		"--disabled",
-		"--nth=3..",
-		cmd.prompt,
-		"--preview-label='content'",
-		string.format("--preview='%s'", cmd.prev),
-		string.format("--preview-window='%s'", cmd.prev_window),
-		"--bind='start:reload:" .. cmd.grep .. " {q}'",
-		"--bind='change:reload:sleep 0.1; " .. cmd.grep .. " {q} || true'",
-		"--bind='ctrl-]:change-preview-window(80%|66%)'",
-		"--bind='ctrl-\\:change-preview-window(right|up)'",
-		"--bind='ctrl-r:clear-query+reload:" .. cmd.grep .. " {q} || true'",
-		"--bind='ctrl-o:execute:$EDITOR {1} +{2}'",
-		string.format(
-			"--bind='alt-c:change-preview-label(content)+change-preview-window(~3,+{2}+3/2,up)+change-preview:%s'",
-			cmd.prev
-		),
-		string.format(
-			"--bind='alt-m:change-preview-label(metadata)+change-preview-window(~5)+change-preview(%s)'",
-			eza_preview("meta", opts)
-		),
-		opts.fzf,
-	}
-
-	-- fzf match option is only available for `rg`
+	-- add `fzf` match option for `rg`
 	if cmd.fzf_match then
-		table.insert(fzf_tbl, cmd.fzf_match(cmd.grep))
+		table.insert(cmd.specific_options, cmd.fzf_match(cmd.grep))
 	end
+
+	local fzf_tbl = build_from_fzf_base(cmd.grep, cmd.prev, cmd.prev_window, cmd.prompt, user_opts, cmd.specific_options)
 
 	return table.concat(fzf_tbl, " ")
 end
 
 -- fzf with `fd` search
-local function build_search_by_name(search_type, opts)
+local function build_search_by_name(search_type, user_opts)
 	local sh = get_sh_helper()
 	local cmd_tbl = {
-		all = sh.wrap("fd --type=d " .. opts.fd .. " {q}; fd --type=f " .. opts.fd .. " {q}"),
-		cwd = sh.wrap("fd --max-depth=1 --type=d " .. opts.fd .. " {q}; fd --max-depth=1 --type=f " .. opts.fd .. " {q}"),
-		dir = "fd --type=dir " .. opts.fd .. " {q}",
-		file = "fd --type=file " .. opts.fd .. " {q}",
+		all = sh.wrap(string.format("fd --type=d %s {q}; fd --type=f %s {q}", user_opts.fd, user_opts.fd)),
+		cwd = sh.wrap(
+			string.format("fd --max-depth=1 --type=d %s {q}; fd --max-depth=1 --type=f %s {q}", user_opts.fd, user_opts.fd)
+		),
+		dir = "fd --type=dir " .. user_opts.fd .. " {q}",
+		file = "fd --type=file " .. user_opts.fd .. " {q}",
 	}
 
 	local fd_cmd = cmd_tbl[search_type]
@@ -160,51 +172,38 @@ local function build_search_by_name(search_type, opts)
 		return nil
 	end
 
-	local bat_prev = "bat --color=always " .. opts.bat .. " {}"
-	local default_prev = string.format("test -d {} && %s || %s", sh.wrap(eza_preview("default", opts)), bat_prev)
+	local bat_prev = "bat --color=always " .. user_opts.bat .. " {}"
+	local default_prev = string.format("test -d {} && %s || %s", sh.wrap(eza_preview("default", user_opts)), bat_prev)
 
-	-- fzf match option <ctrl-s>
-	local bind_fzf_match_tmpl = "--bind='ctrl-s:transform:%s "
-		.. [[echo "rebind(change)+change-prompt(fd> )+clear-query+reload:%s" %s ]]
-		.. [[echo "unbind(change)+change-prompt(fzf> )+clear-query"']]
-
-	local fzf_tbl = {
-		"fzf",
-		"--ansi",
-		"--no-multi",
-		"--reverse",
-		"--no-sort",
-		"--prompt='fd> '",
-		"--preview-label='content'",
-		"--preview-window=up,66%",
-		string.format("--preview='%s'", default_prev),
-		string.format("--bind='start:reload:%s'", fd_cmd),
-		string.format("--bind='change:reload:sleep 0.1; %s || true'", fd_cmd),
-		"--bind='ctrl-]:change-preview-window(80%|66%)'",
-		"--bind='ctrl-\\:change-preview-window(right|up)'",
-		"--bind='ctrl-r:clear-query+reload:" .. fd_cmd .. "'",
+	local specific_options = {
 		"--bind='ctrl-o:execute:$EDITOR {1}'",
-		string.format("--bind='alt-c:change-preview-label(content)+change-preview:%s'", default_prev),
-		string.format("--bind='alt-m:change-preview-label(metadata)+change-preview:%s'", eza_preview("meta", opts)),
-		string.format(bind_fzf_match_tmpl, sh.fd_prompt.cond, fd_cmd, sh.fd_prompt.op),
-		opts.fzf,
+		string.format(
+			"--bind='ctrl-s:transform:%s "
+				.. [[echo "rebind(change)+change-prompt(fd> )+clear-query+reload:%s" %s ]]
+				.. [[echo "unbind(change)+change-prompt(fzf> )+clear-query"']],
+			sh.fd_prompt.cond,
+			fd_cmd,
+			sh.fd_prompt.op
+		),
 	}
+
+	local fzf_tbl = build_from_fzf_base(fd_cmd, default_prev, "up,66%", "fd> ", user_opts, specific_options)
 
 	return table.concat(fzf_tbl, " ")
 end
 
 local function entry(_, job)
 	local _permit = ya.hide()
-	local custom_opts = get_custom_opts() -- from user setup
+	local user_opts = get_user_opts() -- from user setup
 	local cwd = tostring(get_cwd())
 
 	local search_type, search_opt = job.args[1], job.args[2]
 	local args
 
 	if search_type == "content" then
-		args = build_search_by_content(search_opt or "rg", custom_opts) -- fallback to `rg` search
+		args = build_search_by_content(search_opt or "rg", user_opts) -- fallback to `rg` search
 	elseif search_type == "name" then
-		args = build_search_by_name(search_opt or "all", custom_opts) -- fallback to `fd` "all" search (dirs and files)
+		args = build_search_by_name(search_opt or "all", user_opts) -- fallback to `fd` "all" search (dirs and files)
 	else
 		return fail("Please specify either 'content' or 'name' as the first argument")
 	end
